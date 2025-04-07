9fa921d811c81d7bc1393bab54a7fd133ca9658e
Ys Nyang <did20031@naver.com>
2025-04-07 03:08:25 +0000

    함수 활용 및 myif 수정

--- myif.py
+++ myif.py
@@ -5,25 +5,53 @@
 # 만약에 2를 입력하면 2번 캐릭터 출력
 # 만약에 3을 입력하면 3번 캐릭터 출력
 # 잘못 입력하면 잘못 입력했다고 출력
-from art import text2art, tprint
-n = int(input("숫자를 입력하세요:"))
 
-if n == 1:
-    print(text2art("1. CAT"))
-    print(" /\\_/\\  ")
-    print("( o.o ) ")
-    print(" > ^ <  ")
-elif n == 2:
-    print(text2art("2. DOG"))
-    print(" /\\__  ")
-    print(" (    @\\___ ")
-    print(" /         O ")
-    print("/   (_____ /")
-    print("/_____/   U")
-elif n == 3:
-    print(text2art("3. RABBIT"))
-    print("(\\(\\  ")
-    print("( -.-) ")
-    print("o_('')('')")
-else:
-    print("올바른 숫자 입력값이 아닙니다.")
+# 실행 전 pip install art 수동 실행
+
+# pip install 자동 설치
+import subprocess
+import sys
+
+# art 모듈 설치 시도
+try:
+    from art import text2art, tprint
+except ImportError:
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "art"])
+
+from art import text2art, tprint
+
+def play(n):
+    if n == 1:
+        print(text2art("1. CAT"))
+        print(" /\\_/\\  ")
+        print("( o.o ) ")
+        print(" > ^ <  ")
+    elif n == 2:
+        print(text2art("2. DOG"))
+        print(" /\\__  ")
+        print(" (    @\\___ ")
+        print(" /         O ")
+        print("/   (_____ /")
+        print("/_____/   U")
+    elif n == 3:
+        print(text2art("3. RABBIT"))
+        print("(\\(\\  ")
+        print("( -.-) ")
+        print("o_('')('')")
+    else:
+        print("올바른 숫자 입력값이 아닙니다.")
+
+# 1. 5번 반복하는 부분
+print("▶ 5번 반복 실행 모드 시작!")
+for i in range(5):
+    n = int(input("숫자를 입력하세요 (1: 고양이, 2: 강아지, 3: 토끼): "))
+    play(n)
+
+# 2. 0 입력 시 종료되는 무한 반복 모드
+print("\n▶ 0을 입력하면 종료되는 무한 반복 모드 시작!")
+while True:
+    n = int(input("숫자를 입력하세요 (1: 고양이, 2: 강아지, 3: 토끼, 0: 종료): "))
+    if n == 0:
+        print("프로그램을 종료합니다.")
+        break
+    play(n)
---
+++ test3.py
@@ -0,0 +1,13 @@
+def print_cat():
+    cat = [
+        " /\\_/\\  ",
+        "( o.o ) ",
+        " > ^ <  "
+    ]
+
+    for line in cat:
+        print(line)
+
+for i in range(1,11):
+    print(i)
+    print_cat()
